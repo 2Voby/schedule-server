705dbee7e3b4ef10d44901cb60cb3ebc10a93ca5
Vlad Gerasimchuk <88591743+2Voby@users.noreply.github.com>
2023-04-21 09:26:43 +0300

    Update user-model.js

--- src/user-model.js
+++ src/user-model.js
@@ -2,12 +2,7 @@ const mongoose = require("mongoose");
 const { use } = require("express/lib/router");
 
 const userSchema = new mongoose.Schema({
-  Schedule: JSON,
-  ScheduleBell: JSON,
-  TeachersList: JSON,
-  CompliedSchedule: JSON,
-  ExtraClasses: JSON,
-  TimeManagementData: JSON,
+  name: string
 });
 
 module.exports = mongoose.model("User", userSchema);
