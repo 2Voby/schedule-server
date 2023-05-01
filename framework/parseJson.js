292dd611504ea166b89e9e26127f702215520bae
Vlad Gerasimchuk <88591743+2Voby@users.noreply.github.com>
2023-05-01 17:43:21 +0300

    Update parseJson.js

--- framework/parseJson.js
+++ framework/parseJson.js
@@ -7,10 +7,9 @@ module.exports = (req, res) => {
 
   // Проверяем, что источник запроса разрешен
   if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
     res.writeHead(200, {
       "Content-type": "application/json",
-      // "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": `${origin}`,
       "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE",
     });
   }
