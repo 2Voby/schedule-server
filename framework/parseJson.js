91f6e958a54917968fdde869be1db4490e06ce4c
Vlad Gerasimchuk <88591743+2Voby@users.noreply.github.com>
2023-05-01 17:22:18 +0300

    Add files via upload

--- framework/parseJson.js
+++ framework/parseJson.js
@@ -1,7 +1,10 @@
 module.exports = (req, res) => {
   res.writeHead(200, {
     "Content-type": "application/json",
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": [
+      "https://lyceum-schedule.ztu.edu.ua/",
+      "https://script.google.com/",
+    ],
     "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE",
   });
   res.send = (data) => {
