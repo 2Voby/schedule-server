ddf67f939cca556b82a022bfeee88d679e12ae23
Vlad Gerasimchuk <88591743+2Voby@users.noreply.github.com>
2023-05-31 19:15:46 +0300

    Add files via upload

---
+++ framework/parseJson.js
@@ -0,0 +1,20 @@
+module.exports = (req, res) => {
+  const allowedOrigins = [
+    "https://lyceum-schedule.ztu.edu.ua/",
+    "https://script.google.com/",
+  ];
+  const origin = req.headers.origin;
+
+  // Проверяем, что источник запроса разрешен
+  if (allowedOrigins.includes(origin)) {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+      "Access-Control-Allow-Origin": `${origin}`,
+      "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE",
+    });
+  }
+
+  res.send = (data) => {
+    res.end(JSON.stringify(data));
+  };
+};
